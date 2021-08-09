0c193e91d7184f20c4810114d5a24c3e30b05483
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-08-09 21:03:23 +0700

    day 2

---
+++ day 2/index.js
@@ -0,0 +1,189 @@
+// math operator
+/*
+  5 + 9 = 14, 'text' + 'text = texttext
+  '55' + 55 = 110 = 5555
+  
+  '55' - 55 = 5555
+  -
+  /
+  *
+  **
+  %
+ */
+
+// console.log('55' - 55) // 0
+// console.log(5 ** 2) // 25
+// console.log(5 % 2) // 3,1 1
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 5 - 4 = 1
+
+// Logical operator
+/*
+  ==
+  ===
+  !
+  !=
+  >=
+  <=
+  <
+  >
+
+*/
+
+// 5 == 5 // true
+// '5' == 5 //
+// console.log('5' == 5); // true
+// console.log(5 === 5); // true
+// console.log(!(5 === 5))  //false
+// console.log(5 != 5); // false
+// console.log('5' !== 5); // true
+// console.log(!('5' === 5))
+// console.log(5 >= 5)  // true
+
+// const bl = true
+// const bl = false
+// const bl = 'false'
+// const bl ;
+// const bl = x + 55; // NaN
+// x // undefined + 55 = NaN
+// const x;
+
+// const x = '';
+/*
+truly => true 555 'sadas' 
+falsy => undefined null NaN 0 '' false
+*/
+
+// true = 1
+// false = 0
+// && = *
+// || = +
+
+// true false true
+// 1 * 0 + 1 = 1 = true
+
+// if ((5 > 4) && !(5 <= 10) || true) {
+//   console.log('55')
+// }
+
+// 0 false , false
+// 0 * 1 + 0  * 1 = 0
+// if (('55' === 55 && !(55 - '55')) || (55 <= 10 && !false)) {
+//   console.log('55');
+// }
+
+// 1 2 3, 1 2 3
+// if (5 !== 5) {
+//   console.log(1);
+
+// } else if (6 < 5) {
+//   console.log(2);
+
+// } else if (3 < 5) {
+//   console.log(3);
+
+// } else {
+//   console.log(4);
+// }
+
+// if (10 + 10 > 21) {
+//   console.log(1);
+// } else if (true) {
+//   console.log(2);
+// }
+
+// 1 3
+
+// if (5 === 5) {
+//   console.log(1)
+// } else {
+//   console.log(2)
+// }
+
+// if (1 > 0) {
+//   console.log(3)
+// }
+
+// if (5 > 3) {
+//   console.log(1);
+
+//   if (4 < 3) {
+//     console.log(2);
+//   } else {
+//     console.log(3);
+//   }
+
+//   if (5 === '5') {
+//     console.log(4);
+//   } else {
+//     console.log(5);
+
+//     if (5 == 5) {
+//       console.log(6);
+//     }
+//   }
+// }
+
+// 1 3 5 6
+
+// tai 1 3 5 6
+// 1 5 6
+
+// if (true) {
+//   console.log('false');
+// }
+
+// break
+
+// if (true) console.log(55);
+
+// if (true) {
+//   console.log(1);
+//   console.log(2);
+// }
+
+// var user = '';
+// if (!user) {
+//   user = 'tai';
+// } else {
+//   user = 'liew';
+// }
+
+// ternary condition
+// var user = !user ? 'tai' : 'liew';
+
+// console.log(user); // liew //tai
+
+// lv 1
+// x = 3 // 
+// let x = 1
+
+// const a = 5
+// a = 6
+// a = 5 + 10
+// a = x
+
+
+// if (true) {
+//   // lv 2
+//   let y = 2
+// }
+
+
+// lv1
+// console.log(a)
+// console.log(y)
+
+// const 
+
+// let
+
+// $php = 5
+// $php = 6
+
+let x = 5
+x = 6
+
+// const 
