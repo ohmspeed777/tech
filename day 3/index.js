2331bf8b2231d511cf4043be84ea76d2856bfd03
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-08-11 19:59:01 +0700

    day 4

---
+++ day 3/index.js
@@ -0,0 +1,67 @@
+// console.log(1)
+// console.log(2)
+// console.log(4)
+// console.log(4)
+// console.log(5)
+
+// while
+// let index = 1;
+// 1 true
+// 1
+// 2 true
+// 2
+// 3
+// 4
+// 5
+// 6
+
+
+
+
+
+// 1
+// 2
+// 3
+// 4
+// 5
+let index = 1;
+
+while (index <= 5) {
+  console.log(index);
+  index = index + 1;
+}
+
+do {
+  console.log(index);
+  index = index + 1;
+} while (index <= 5);
+
+// 5 / 2 = 2 / 1
+// 4 / 2 = 2 / 0
+
+// 10 / 2 = 5 / 0
+// 21 / 2 = 10 / 1
+
+// 5 % 2
+
+// for (let i = 1; i <= 100; i = i + 1) {
+//   // 1 % 2 = 1
+//   // 2 % 2 = 0
+//   // 3 % 2 = 1
+//   // 4 % 2 = 0
+
+//   if (i % 2 === 0) {
+//     console.log(i)
+//   }
+//   // 1 / 2  =>  1
+//   // 2 / 2 => 0
+//   // 3 / 2 = 1
+//   // 4 / 2 = 0
+//   // 8 / 2 = 0 
+// }
+
+// let i = 1;
+// i + 1
+// i = i + 1
+
+// 1 - 100 // 51 52 53 54 55 56 57 58 59 60 .... 100 // if 
