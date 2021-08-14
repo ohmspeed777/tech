badd785ea6612bf3715937ab99c4c9b433b3c093
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-08-14 21:24:35 +0700

    day 7

---
+++ day 7/index.js
@@ -0,0 +1,112 @@
+let numArray = [22, 90, 45, 50, 63, 70, 85, 68, 58];
+
+/*
+80 A
+70 B
+60 C
+50 D
+1-49 F
+
+Array position index 0 is F
+Array position index 1 is F
+.
+.
+.
+.
+.
+
+*/
+
+// function
+// tree number person
+
+// function calculateCircle() {
+//   console.log((22 / 7) * 5 ** 2);
+// }
+
+// calculateCircle()
+
+// console.log((22 / 7) * (10 ** 2));
+// console.log((22 / 7) * (10 ** 2));
+// console.log((22 / 7) * (10 ** 2));
+// console.log((22 / 7) * (10 ** 2));
+// console.log((22 / 7) * (10 ** 2));
+// console.log((22 / 7) * (10 ** 2));
+// undefined
+
+// parameter
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// function concatStr(str1, str2) {
+//   console.log(str1 + str2);
+// }
+
+// // arguments
+// add(5, 3);
+// add(6, 3);
+// add(10, 3);
+
+// concatStr('function', 5);
+// add();
+// add();
+// add();
+// add();
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function calcTriangle(base, hight) {
+//   // lv 2
+//   let result = base * hight * 0.5;
+//   return result;
+// }
+
+// // lv 1
+// // undefined
+// const r = calcTriangle(10, 10);
+// console.log(r);
+// console.log(result)
+// console.log(calcTriangle(10, 10));
+
+/*
+สร้างฟั้่งชั่น คำนวนพื้นที่สี่เหลี่ยมจตุรัส
+โดยมี พารามิเตอร์ 1 ตัว และค่่าของ อากิวเม้น คือ 8
+
+console.log(ชื่อฟังชั่น(8))
+// 64
+
+
+*/
+
+/*
+let scores = [50, 75, 38, 49, 62, 82]
+
+function matchGrade(scores) {
+
+}
+
+function displayGrade(grade) {
+  console.log('this person grade is' + grade)
+}
+
+this person grade is D
+this person grade is B
+this person grade is F
+this person grade is F
+this person grade is C
+this person grade is A
+
+
+
+*/
+
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//   add(5, i);
+// }
